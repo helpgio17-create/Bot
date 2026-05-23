d1b97e93889eea6d94452cc4950e67c695269762
helpgio17-create <helpgio17@gmail.com>
2026-05-23 09:59:18 +0530

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Telegram Bot Token — @BotFather se lo
-export TELEGRAM_BOT_TOKEN="7384442199:AAFDFWROw7orPM_D3I0xes2lLeq7a1chIhs"
+export TELEGRAM_BOT_TOKEN="8531564509:AAH9u3IcljH_nAt7DW1Yd3fnwgH4DO0us-E"
 
 # Admin Telegram User ID
 export ADMIN_USER_ID="7265678519"
